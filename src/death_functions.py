362a2d31c96d23875bbb1e45e199948b899d2ca7
Mikhail "ArdRaeiss" Merzlyutin <merzlyutinme@gmail.com>
2019-05-22 00:26:54 +0300

    Fix dead monsters render order
    Took 9 minutes

--- src/death_functions.py
+++ src/death_functions.py
@@ -2,6 +2,7 @@ import tcod
 
 from game_messages import Message
 from game_states import GameStates
+from render_functions import RenderOrder
 
 
 def kill_player(player):
@@ -21,5 +22,6 @@ def kill_monster(monster):
     monster.ai = None
     monster.name = 'Remains of ' + monster.name
     monster.blocks_movement = False
+    monster.render_order = RenderOrder.CORPSE
 
     return death_message
